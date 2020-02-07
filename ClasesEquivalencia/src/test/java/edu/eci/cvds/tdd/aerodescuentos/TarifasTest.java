0b101f414e792a2d79f6087bfaa4b7bd31430afb
unknown <verbo.camacho-v@labinfo.is.escuelaing.edu.co>
2020-02-07 15:34:22 -0500

    LAB 03 FINAL

--- ClasesEquivalencia/src/test/java/edu/eci/cvds/tdd/aerodescuentos/TarifasTest.java
+++ ClasesEquivalencia/src/test/java/edu/eci/cvds/tdd/aerodescuentos/TarifasTest.java
@@ -8,7 +8,6 @@ import org.junit.Test;
 public class TarifasTest {
     private CalculadorDescuentos cal = new CalculadorDescuentos();
 
-    //String name, int id, int age, Gender gender, boolean alive, boolean military
     /**
     @param tarifaBase valor base del vuelo
     @param diasAntelacion dias de antelación del vuelo
@@ -16,16 +15,27 @@ public class TarifasTest {
     @throws ExcepcionParametrosInvalidos [XXXXXXXXXXXXXXXXXXXXXXXXXXX]
     **/
     
+    @Test
+    public void deberiaSerValidoEdadMayorMenorEdad() {
+        Assert.assertEquals(1000.0, (cal.calculoTarifa(1000,0,19)),0.0);
+    }
+    @Test
+    public void deberiaSerValidoEdadMayorLimite() {
+        Assert.assertEquals(920.0, (cal.calculoTarifa(1000,0,165)),0.0);
+    }
+    @Test
+    public void deberiaSerValidoEdadMayor() {
+        Assert.assertEquals(1000.0, (cal.calculoTarifa(1000,0,65)),0.0);
+    }
+    
     /**
-     */
+     * No hay edad negativa. Deberia arrojar Execption
+     **/
     @Test
     public void deberiaSerMenorValidoSingle() {
-        Assert.assertEquals(1000.0, (cal.calculoTarifa(1000,0,19)),0.0);
-        Assert.assertEquals(920.0, (cal.calculoTarifa(1000,0,165)),0.0);
-        Assert.assertEquals(1000.0, (cal.calculoTarifa(1000,0,65)),0.0);
-        //No hay edad negativa. Arrojaria Execption
-        Assert.assertEquals(1000.0, (cal.calculoTarifa(1000,0,18)),0.0);
+        Assert.assertNotEquals(1000.0, (cal.calculoTarifa(1000,0,-18)),0.0);
     }
+    
     @Test
     public void deberiaSerMenorValido() {
         Assert.assertEquals(950.0, (cal.calculoTarifa(1000,0,17)),0.0);
@@ -47,8 +57,37 @@ public class TarifasTest {
         Assert.assertEquals(800.0, (cal.calculoTarifa(1000,21,0)),0.0);
     }
     @Test
-    public void deberiaSerMismoDias() {
-        Assert.assertEquals(950.0, (cal.calculoTarifa(1000,20,2)),0.0);
+    public void deberiaSerEdadMayor65() {
+        Assert.assertEquals(920.0, (cal.calculoTarifa(1000,12,66)),0.0);
     }
-   
+    @Test
+    public void deberiaSerEdad65() {
+        Assert.assertEquals(1000.0, (cal.calculoTarifa(1000,12,65)),0.0);
+    }
+    @Test
+    public void deberiaSerDiferenteDias20() {
+        Assert.assertNotEquals(920.0, (cal.calculoTarifa(1000,20,66)),0.0);
+    }
+    @Test
+    public void deberiaServalidodiasantelacionyedadmenor() {
+        Assert.assertEquals(770.0, (cal.calculoTarifa(1000,22,67)),0.0);
+    }
+    
+    /**
+    Nos damos cuenta que existe un error en la libreria, debido a que cuando los dias de Antelacion es 20
+    no toma ningun caso referente a la edad
+    **/
+    @Test
+    public void deberiaSerDiferenteDias20ymenor() {
+        Assert.assertNotEquals(950.0, (cal.calculoTarifa(1000,20,16)),0.0);
+    }
+    
+    @Test
+    public void deberiaSerDiasMayor20() {
+        Assert.assertEquals(850.0, (cal.calculoTarifa(1000,22,19)),0.0);
+    }
+    @Test
+    public void deberiaSerinvalido() {
+        Assert.assertNotEquals(0.0, (cal.calculoTarifa(1000,22,-1)),0.0);
+    }
 }
